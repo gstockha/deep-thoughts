4f0e18b06165596032e21c75ac031a53c64323bc
George Stockhammer <sunchipzzz@gmail.com>
2022-02-10 12:31:28 -0600

    routes and hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
-  uri: '/graphql', //changed in the package.json 'proxy' setting
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
@@ -18,13 +28,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,6 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-
-  type Reaction { #replies to a single thought
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-  }
-
   type User {
     _id: ID
     username: String
@@ -28,16 +10,32 @@ const typeDefs = gql`
     friends: [User]
   }
 
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
   type Auth {
     token: ID!
     user: User
   }
-  
+
   type Query {
     me: User
     users: [User]
-    user(username: String!): User #! means that data must exist
-    thoughts(username: String): [Thought] #where we don't need a username for it to work
+    user(username: String!): User
+    thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
 
@@ -48,8 +46,6 @@ const typeDefs = gql`
     addReaction(thoughtId: ID!, reactionBody: String!): Thought
     addFriend(friendId: ID!): User
   }
-
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
